2d4f55242a035d6924afb4d5efa32d19e547fd20
Jerome Navez <ext-jena@hms-networks.com>
2024-04-12 09:25:50 +0200

    Update indentation from spaces to tabs in parser files
    All indentation in the madge_parser.py and jdeps_parser.py files has been updated from spaces to tabs. This change applies to both function definitions and comments, improving consistency across the codebase.

--- jdeps_parser.py
+++ jdeps_parser.py
@@ -4,27 +4,27 @@ EDGE_SEPARATOR = " -> "
 
 
 def main():
-    args = sys.argv[1:]
-    file = open(args[0], "r")
-    entries = file.readlines()
-    print(parse(entries))
+	args = sys.argv[1:]
+	file = open(args[0], "r")
+	entries = file.readlines()
+	print(parse(entries))
 
 
 def parse(lines, exclude_regex=None):
-    transformed_lines = []  # List to store the transformed lines
+	transformed_lines = []  # List to store the transformed lines
 
-    for line in lines:
-        # Check if the line is indented, indicating a mapping we're interested in
-        if line.startswith("   "):
-            splitted = line.split(EDGE_SEPARATOR)
-            from_ = splitted[0].strip()
-            to_ = splitted[1].split(' ')[0]
-            if not exclude_regex or (not exclude_regex.match(to_) and not exclude_regex.match(from_)):
-                clean_line = from_ + EDGE_SEPARATOR + to_
-                transformed_lines.append(clean_line)
+	for line in lines:
+		# Check if the line is indented, indicating a mapping we're interested in
+		if line.startswith("   "):
+			splitted = line.split(EDGE_SEPARATOR)
+			from_ = splitted[0].strip()
+			to_ = splitted[1].split(' ')[0]
+			if not exclude_regex or (not exclude_regex.match(to_) and not exclude_regex.match(from_)):
+				clean_line = from_ + EDGE_SEPARATOR + to_
+				transformed_lines.append(clean_line)
 
-    return transformed_lines
+	return transformed_lines
 
 
 if __name__ == "__main__":
-    main()
+	main()
--- madge_parser.py
+++ madge_parser.py
@@ -2,29 +2,29 @@ import sys
 
 
 def main():
-    args = sys.argv[1:]
-    file = open(args[0], "r")
-    entries = file.readlines()
-    print(parse(entries))
+	args = sys.argv[1:]
+	file = open(args[0], "r")
+	entries = file.readlines()
+	print(parse(entries))
 
 
 def parse(lines, exclude_regex=None):
-    parent = None  # Variable to keep track of the current parent node
-    parent_is_model = False  # Variable to keep track of the current parent node
-    relationships = []  # List to store the parent-child relationships
+	parent = None  # Variable to keep track of the current parent node
+	parent_is_model = False  # Variable to keep track of the current parent node
+	relationships = []  # List to store the parent-child relationships
 
-    for line in lines:
-        stripped_line = line.strip()  # Remove leading and trailing whitespace
-        if line.startswith("  ") and not parent_is_model:  # Check if the line is indented (assuming 2 spaces for indentation)
-            # This is a child node, so create a relationship entry
-            if not exclude_regex or not exclude_regex.match(stripped_line):
-                relationships.append(parent + ' -> ' + stripped_line)
-        if line and not line.startswith("  "):
-            parent = stripped_line
-            parent_is_model = exclude_regex and exclude_regex.match(parent)
+	for line in lines:
+		stripped_line = line.strip()  # Remove leading and trailing whitespace
+		if line.startswith("  ") and not parent_is_model:  # Check if the line is indented (assuming 2 spaces for indentation)
+			# This is a child node, so create a relationship entry
+			if not exclude_regex or not exclude_regex.match(stripped_line):
+				relationships.append(parent + ' -> ' + stripped_line)
+		if line and not line.startswith("  "):
+			parent = stripped_line
+			parent_is_model = exclude_regex and exclude_regex.match(parent)
 
-    return relationships
+	return relationships
 
 
 if __name__ == "__main__":
-    main()
+	main()
